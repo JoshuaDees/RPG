9fee7059d106b5bd2cf14c0663accc7663118d76
Joshua Dees <dees.joshua@gmail.com>
2020-04-09 21:13:34 -0400

    chore(dev): Moves all dev items to dev directory

--- admin/scripts/Users.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS `Users` (
-  `UserId` int(11) NOT NULL AUTO_INCREMENT,
-  `UserName` varchar(50) NOT NULL,
-  `UserEmail` varchar(50) NOT NULL,
-  `UserPassword` varchar(50) NOT NULL,
-  PRIMARY KEY (`UserId`)
-)
