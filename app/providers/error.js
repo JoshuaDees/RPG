ce49cb4b9ae74ef4bfd5de9efb91b3291271ba66
Joshua Dees <dees.joshua@gmail.com>
2020-04-11 00:48:52 -0400

    feat(theme): Fixes minor issues with the theme

--- app/providers/error.js
+++ app/providers/error.js
@@ -4,13 +4,13 @@ angular
     return new function() {
       this.alert = function(error) {
         $(document.body).append(
-          '<dialog id="error">' +
+          '<dialog modal id="error">' +
             '<header>' +
               '<a onclick="$(\'#error\').remove();" ui-sref><i class="fa fa-times"></i></a>' +
               'Error' +
             '</header>' +
             '<main>' + error + '</main>' +
-            '<footer><button onclick="$(\'#error\').remove();">Close</button></footer>' +
+            '<footer><button onclick="$(\'#error\').remove();" type="submit">Close</button></footer>' +
           '</dialog>'
         );
 
--- dist/rpg.js
+++ dist/rpg.js
@@ -48,13 +48,13 @@
       return new function() {
         this.alert = function(error) {
           $(document.body).append(
-            '<dialog id="error">' +
+            '<dialog modal id="error">' +
               '<header>' +
                 '<a onclick="$(\'#error\').remove();" ui-sref><i class="fa fa-times"></i></a>' +
                 'Error' +
               '</header>' +
               '<main>' + error + '</main>' +
-              '<footer><button onclick="$(\'#error\').remove();">Close</button></footer>' +
+              '<footer><button onclick="$(\'#error\').remove();" type="submit">Close</button></footer>' +
             '</dialog>'
           );
 
